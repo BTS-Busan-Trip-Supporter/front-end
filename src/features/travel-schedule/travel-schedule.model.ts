d34658859b314231ec01d985c190f7cb1a5b7c1f
cjeongmin <cjeongmin27@gmail.com>
2024-08-03 21:14:16 +0900

    feat: Add type declarations related to travel schedule

---
+++ src/features/travel-schedule/travel-schedule.model.ts
@@ -0,0 +1,57 @@
+export interface Destination {
+  id: string;
+  name: string;
+  timeToDestination: number;
+  startDate: Date;
+  endDate: Date;
+}
+
+export class DaySchedule {
+  destinations: Destination[];
+
+  constructor({ destinations }: Pick<DaySchedule, 'destinations'>) {
+    this.destinations = destinations;
+  }
+
+  addDestination({ destination }: { destination: Destination }) {
+    this.destinations.push(destination);
+    this.sortByStartTime();
+  }
+
+  removeDestination({ destinationId }: { destinationId: string }) {
+    this.destinations = this.destinations.filter(
+      ({ id }) => id !== destinationId,
+    );
+  }
+
+  updateDestination({
+    destinationId,
+    updateValue,
+  }: {
+    destinationId: string;
+    updateValue: Partial<Destination>;
+  }) {
+    this.destinations = this.destinations.map((destination) =>
+      destination.id === destinationId
+        ? { ...destination, ...updateValue }
+        : destination,
+    );
+    this.sortByStartTime();
+  }
+
+  sortByStartTime() {
+    this.destinations.sort((lhs, rhs) => {
+      const [lhsStartTime, rhsStartTime] = [
+        lhs.startDate.getTime(),
+        rhs.startDate.getTime(),
+      ];
+      if (lhsStartTime < rhsStartTime) return -1;
+      else if (lhsStartTime === rhsStartTime) return 0;
+      return 1;
+    });
+  }
+}
+
+export interface TravelSchedule {
+  schedules: DaySchedule[];
+}
