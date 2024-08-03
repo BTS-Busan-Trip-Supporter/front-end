316e26c2debdc89ecac04ef80070edd8b5f90e97
cjeongmin <cjeongmin27@gmail.com>
2024-08-03 21:39:09 +0900

    test: Add test for travel schedule logic

---
+++ src/__test__/travel-schedule-actions.test.ts
@@ -0,0 +1,71 @@
+import {
+  createTravelScheduleStore,
+  Destination,
+} from '@/features/travel-schedule';
+
+describe('여행 관련 일정 로직 테스트', () => {
+  test('초기 상태는 반드시 비어 있는 배열이어야 합니다.', () => {
+    const store = createTravelScheduleStore();
+
+    const schedules = store.getState().schedules;
+
+    expect(schedules.length).toEqual(0);
+  });
+
+  test('여행 일자를 추가하였을 때, 정상적으로 추가되어야 합니다.', () => {
+    const store = createTravelScheduleStore();
+
+    const { schedules, addDaySchedule } = store.getState();
+    addDaySchedule();
+
+    expect(store.getState().schedules.length).not.toEqual(schedules.length);
+  });
+
+  test('목적지를 추가하였을 때, 정상적으로 추가되어야 합니다.', () => {
+    const store = createTravelScheduleStore();
+
+    const destination: Destination = {
+      id: 'test-id',
+      name: 'test-name',
+      timeToDestination: -1,
+      startDate: new Date(),
+      endDate: new Date(),
+    };
+    const { addDaySchedule, addDestination } = store.getState();
+    addDaySchedule();
+    addDestination({
+      day: 0,
+      destination,
+    });
+
+    const target = store
+      .getState()
+      .schedules[0].destinations.find(({ id }) => id === 'test-id');
+    expect(target).not.toBeUndefined();
+  });
+
+  test('목적지를 삭제하였을 때, 정상적으로 제거되어야 합니다.', () => {
+    const store = createTravelScheduleStore();
+
+    const destination: Destination = {
+      id: 'test-id',
+      name: 'test-name',
+      timeToDestination: -1,
+      startDate: new Date(),
+      endDate: new Date(),
+    };
+    const { addDaySchedule, addDestination, removeDestination } =
+      store.getState();
+    addDaySchedule();
+    addDestination({
+      day: 0,
+      destination,
+    });
+    removeDestination({ day: 0, destination });
+
+    const target = store
+      .getState()
+      .schedules[0].destinations.find(({ id }) => id === 'test-id');
+    expect(target).toBeUndefined();
+  });
+});
