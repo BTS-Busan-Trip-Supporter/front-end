3e535bffe061857ad196d42d2f2dd7db773558b6
Cho-heejung <66050038+he2e2@users.noreply.github.com>
2024-08-03 12:19:43 +0900

    Feat: Add default layout and navigation bar (#7)
    * feat: add default layout and nav bar (#1)
    
    * feat: add routes
    
    * fix: modified condition on the navigation bar

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import styled from '@emotion/styled';
+import { usePathname, useRouter } from 'next/navigation';
+
+const styles = {
+  container: styled.nav`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 1.5rem 0;
+    position: fixed;
+    bottom: 0;
+    background-color: white;
+  `,
+
+  menu: styled.span<NavProps>`
+    color: ${(props) => (props.$active ? '#605EFF' : '#b0afbc')};
+
+    font-family: 'Noto Sans KR';
+    font-size: 0.75rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    gap: 0.2rem;
+  `,
+
+  icon: styled.img`
+    width: 1.75rem;
+    object-fit: content;
+  `,
+};
+
+interface NavProps {
+  $active?: boolean;
+}
+
+export function NavigationBar() {
+  const pathName = usePathname();
+  const router = useRouter();
+
+  if (pathName === '/login' || pathName === '/my') {
+    return null;
+  }
+
+  return (
+    <styles.container>
+      <div
+        onClick={() => {
+          router.replace('/travel');
+        }}
+      >
+        {pathName.startsWith('/travel') ? (
+          <styles.menu $active>
+            <styles.icon src='./travel-active.png' alt='travel-active-menu' />
+            여행하기
+          </styles.menu>
+        ) : (
+          <styles.menu>
+            <styles.icon src='./travel.png' alt='travel-menu' />
+            여행하기
+          </styles.menu>
+        )}
+      </div>
+      <div
+        onClick={() => {
+          router.replace('/');
+        }}
+      >
+        {pathName === '/' ? (
+          <styles.menu $active>
+            <styles.icon src='./home-active.png' alt='home-active-menu' />홈
+          </styles.menu>
+        ) : (
+          <styles.menu>
+            <styles.icon src='./home.png' alt='home-menu' />홈
+          </styles.menu>
+        )}
+      </div>
+      <div
+        onClick={() => {
+          router.replace('/record');
+        }}
+      >
+        {pathName.startsWith('/record') ? (
+          <styles.menu $active>
+            <styles.icon src='./record-active.png' alt='record-active-menu' />
+            기록하기
+          </styles.menu>
+        ) : (
+          <styles.menu>
+            <styles.icon src='./record.png' alt='record-menu' />
+            기록하기
+          </styles.menu>
+        )}
+      </div>
+    </styles.container>
+  );
+}
