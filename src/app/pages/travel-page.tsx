02cafa447ee51cd9d4a1f90faa66292e50262ec6
he2e2 <dddd6368@naver.com>
2024-08-08 11:25:51 +0900

    feat: Add heading text

--- src/app/pages/travel-page.tsx
+++ src/app/pages/travel-page.tsx
@@ -5,10 +5,18 @@ import styled from '@emotion/styled';
 import { Background, SearchBox, CustomButton } from '@/components';
 
 export function TravelPage() {
+  const user = {
+    name: '최보윤',
+  };
   return (
     <>
       <Background page='travel' />
       <styles.container>
+        <h2>
+          {user.name}님,
+          <br />
+          어디가실 계획인가요?
+        </h2>
         <SearchBox />
         <styles.btnCon>
           <CustomButton color='#FF75C8' text='알아서 해줘' />
@@ -29,6 +37,17 @@ const styles = {
     justify-content: center;
     align-items: center;
     gap: 1.69rem;
+
+    h2 {
+      color: #505050;
+      font-family: 'Noto Sans KR';
+      font-size: 1.4375rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+      letter-spacing: -0.02875rem;
+      text-align: center;
+    }
   `,
   btnCon: styled.div`
     width: 100%;
