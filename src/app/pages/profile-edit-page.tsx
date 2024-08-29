b1cc55a763b0fdfa4a11a6ca38597aa97aadb121
he2e2 <dddd6368@naver.com>
2024-08-29 15:02:45 +0900

    feat: Added profile-edit-page layout

---
+++ src/app/pages/profile-edit-page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+export function ProfileEditPage() {
+  return <styles.container />;
+}
+
+const styles = {
+  container: styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    background-color: #fafaff;
+  `,
+};
