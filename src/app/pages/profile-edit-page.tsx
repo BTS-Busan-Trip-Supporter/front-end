536117f1dcb22b1e20b47fbc5368500d35ed0582
he2e2 <dddd6368@naver.com>
2024-08-29 16:40:45 +0900

    feat: Added profile edit section header

--- src/app/pages/profile-edit-page.tsx
+++ src/app/pages/profile-edit-page.tsx
@@ -1,9 +1,33 @@
 'use client';
 
 import styled from '@emotion/styled';
+import { useRouter } from 'next/navigation';
 
 export function ProfileEditPage() {
-  return <styles.container />;
+  const router = useRouter();
+  return (
+    <styles.container>
+      <Header
+        h2='프로필 편집'
+        prevButtonHandler={() => router.replace('/my')}
+      />
+    </styles.container>
+  );
+}
+
+function Header({
+  h2,
+  prevButtonHandler,
+}: {
+  h2: string;
+  prevButtonHandler: () => void;
+}) {
+  return (
+    <styles.header>
+      <styles.prevButton onClick={prevButtonHandler} />
+      <h2>{h2}</h2>
+    </styles.header>
+  );
 }
 
 const styles = {
@@ -14,5 +38,41 @@ const styles = {
     flex-direction: column;
     align-items: center;
     background-color: #fafaff;
+  `,
+
+  header: styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1.9rem;
+
+    width: 100%;
+    position: relative;
+
+    border: 1px solid red;
+
+    h2 {
+      color: #434343;
+
+      text-align: center;
+      font-family: 'Noto Sans KR';
+      font-size: 1.1875rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+      letter-spacing: -0.02375rem;
+    }
+  `,
+
+  prevButton: styled.button`
+    background: url('/chevron-left.svg') no-repeat center;
+    background-size: content;
+    width: 1.125rem;
+    height: 1.125rem;
+    position: absolute;
+    border: none;
+    left: 1rem;
+    top: 50%;
+    transform: translateY(-50%);
   `,
 };
