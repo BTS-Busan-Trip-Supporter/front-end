1c8495a38dc518875e8be093ac58d69c3c3a6e83
he2e2 <dddd6368@naver.com>
2024-08-29 20:33:57 +0900

    feat: Added ProfileSection

--- src/app/pages/profile-edit-page.tsx
+++ src/app/pages/profile-edit-page.tsx
@@ -3,6 +3,17 @@
 import styled from '@emotion/styled';
 import { useRouter } from 'next/navigation';
 
+import { Header, ProfileSection } from '@/components/profile';
+import { type User } from '@/features/profile';
+
+const dummyUser: User = {
+  tag: 123,
+  nickname: 'P의 여행자',
+  password: '123456',
+  email: 'pop@naver.com',
+  profileImage: 'https://picsum.photos/50/50',
+};
+
 export function ProfileEditPage() {
   const router = useRouter();
   return (
@@ -11,22 +22,8 @@ export function ProfileEditPage() {
         h2='프로필 편집'
         prevButtonHandler={() => router.replace('/my')}
       />
+      <ProfileSection user={dummyUser} />
     </styles.container>
-  );
-}
-
-function Header({
-  h2,
-  prevButtonHandler,
-}: {
-  h2: string;
-  prevButtonHandler: () => void;
-}) {
-  return (
-    <styles.header>
-      <styles.prevButton onClick={prevButtonHandler} />
-      <h2>{h2}</h2>
-    </styles.header>
   );
 }
 
@@ -38,41 +35,5 @@ const styles = {
     flex-direction: column;
     align-items: center;
     background-color: #fafaff;
-  `,
-
-  header: styled.section`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 1.9rem;
-
-    width: 100%;
-    position: relative;
-
-    border: 1px solid red;
-
-    h2 {
-      color: #434343;
-
-      text-align: center;
-      font-family: 'Noto Sans KR';
-      font-size: 1.1875rem;
-      font-style: normal;
-      font-weight: 500;
-      line-height: normal;
-      letter-spacing: -0.02375rem;
-    }
-  `,
-
-  prevButton: styled.button`
-    background: url('/chevron-left.svg') no-repeat center;
-    background-size: content;
-    width: 1.125rem;
-    height: 1.125rem;
-    position: absolute;
-    border: none;
-    left: 1rem;
-    top: 50%;
-    transform: translateY(-50%);
   `,
 };
---
+++ src/features/profile/profile.type.ts
@@ -0,0 +1,7 @@
+export interface User {
+  tag: number;
+  nickname: string;
+  password: string;
+  email: string;
+  profileImage: string;
+}
